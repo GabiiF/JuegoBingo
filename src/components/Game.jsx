118b67163ccf35e417c3739f96d83ed80bd00256
GabiiF <gabriela.fernandez.92@live.com.ar>
2023-07-07 23:36:48 -0300

    agrego game over

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,25 +1,19 @@
 import React, { useState } from 'react';
 import Button from './Button';
 import WordList from './WordList';
+import palabras from '../Palabras';
 
 const Game = () => {
-  const initialWords = [
-    'medias',
-    'coche',
-    'alfombra de juego',
-    'pañal',
-    'mameluco',
-    'leche de fórmula',
-    'cuna'
-  ];
-
-  const [words, setWords] = useState(initialWords);
+  const [words, setWords] = useState(palabras);
   const [accumulatedWords, setAccumulatedWords] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+
 
   const handleButtonClick = () => {
     if (words.length === 0) {
       // Si ya no quedan palabras en la lista, puedes reiniciar el juego o mostrar un mensaje de finalización.
-      return;
+      setGameOver(true);
+        return;
     }
 
     const randomIndex = Math.floor(Math.random() * words.length);
@@ -33,10 +27,23 @@ const Game = () => {
     setWords(updatedWords);
   };
 
+  const restartGame = () => {
+    setWords(palabras);
+    setAccumulatedWords([]);
+    setGameOver(false);
+  };
+
   return (
     <div>
-      <Button onClick={handleButtonClick} />
-      <WordList words={accumulatedWords} />
+       <Button onClick={handleButtonClick} disabled={gameOver} />
+      {gameOver ? (
+        <p>Juego terminado. Ya no quedan más palabras.</p>
+      ) : (
+        <WordList words={accumulatedWords} />
+      )}
+      {gameOver && (
+        <button onClick={restartGame}>Reiniciar juego</button>
+      )}
     </div>
   );
 };
