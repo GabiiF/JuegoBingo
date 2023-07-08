e7cb82f3ef19f435d929f7eea49afd163672f6b1
GabiiF <gabriela.fernandez.92@live.com.ar>
2023-07-08 05:34:53 -0300

    arreglo ubicacion de palabras en lista

--- src/components/WordList.jsx
+++ src/components/WordList.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import {Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 
 const WordList = ({ words }) => {
   const columnCount = 3; // Número de columnas deseado
   const wordsPerColumn = Math.ceil(words.length / columnCount); // Cantidad de palabras por columna
+  const numeracion = 0;
+
+  const gridTemplateColumns = `repeat(${columnCount}, 1fr)`;
 
   return (
-    <div style={{ display: 'flex'}}>
-      {[...Array(columnCount)].map((_, columnIndex) => (
-        <Typography key={columnIndex} variant="h6" gutterBottom style={{ fontSize: '30px',marginRight: '5%' }}>
-        <ul style={{ flex: 1 }}>
-          {words
-            .slice(columnIndex * wordsPerColumn, (columnIndex + 1) * wordsPerColumn)
-            .map((word, index) => (
-              <li key={index}>{word}</li>
-            ))}
-        </ul>
+    <div style={{ display: 'grid', gridTemplateColumns,marginTop: '1%' }}>
+      {words.map((word, index) => (
+        <Typography key={index} variant="h6" gutterBottom style={{ fontSize: '30px', marginRight: '5%',marginBottom: '1%'}}>
+          <ul style={{ margin: '0px' }}>
+            <li>{`${index + 1}. ${word}`}</li>
+          </ul>
         </Typography>
       ))}
     </div>
