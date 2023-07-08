4fbafe64255f5393644ed73fe6f5dcb4ae6c9f73
GabiiF <gabriela.fernandez.92@live.com.ar>
2023-07-08 03:23:35 -0300

    arreglo estilo de padding y lista para que se ponga en 3 columnas

--- src/components/WordList.jsx
+++ src/components/WordList.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import {Typography } from '@mui/material';
 
 const WordList = ({ words }) => {
-    return (
-      <ul>
-        {words.map((word, index) => (
-          <li key={index}>{word}</li>
-        ))}
-      </ul>
-    );
-  };
+  const columnCount = 3; // Número de columnas deseado
+  const wordsPerColumn = Math.ceil(words.length / columnCount); // Cantidad de palabras por columna
+
+  return (
+    <div style={{ display: 'flex'}}>
+      {[...Array(columnCount)].map((_, columnIndex) => (
+        <Typography key={columnIndex} variant="h6" gutterBottom style={{ fontSize: '30px',marginRight: '5%' }}>
+        <ul style={{ flex: 1 }}>
+          {words
+            .slice(columnIndex * wordsPerColumn, (columnIndex + 1) * wordsPerColumn)
+            .map((word, index) => (
+              <li key={index}>{word}</li>
+            ))}
+        </ul>
+        </Typography>
+      ))}
+    </div>
+  );
+};
+
 export default WordList;
